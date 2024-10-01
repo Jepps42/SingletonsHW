19e78a799a6eff0e052e9904d55f7dec7c441b0b
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-10-01 10:48:06 -0400

    New scene on collision

--- SingletonsHW/Assets/Scripts/GameManager.cs
+++ SingletonsHW/Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables 
+
+    public static GameManager Instance;
+
+    private bool restartScene = true;
+    //Awake is called instead of spawn
+    private void Awake()
     {
-        
+        //A singlton must only have one instance, check to make sure this is the only instance
+        //If it is, make sure its not destroyed, if its not destroy this instance
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        if (restartScene)
+        {
+            SceneManager.LoadScene("SampleScene");
+            restartScene = false;
+        }
     }
 }
--- SingletonsHW/Assets/Scripts/WASD_Capsule.cs
+++ SingletonsHW/Assets/Scripts/WASD_Capsule.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WASD_Capsule : MonoBehaviour
 {
+    //Variables
+    public float speed = 8f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,41 @@ public class WASD_Capsule : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Find the object every frame and make a vector
+        Vector2 pos = transform.position;
+
+
+        //If I press right arrow, make the object move to the right
+        //If is called every time I press the right arrow
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        //If I press left arrow, make the object move left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        //If I press down arrow, make the object go down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        //If I press up arrow, make the object go up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        //Transform position needs to be in the last line for it to function (hiearchy)
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.Instance.StartGame();
     }
 }
